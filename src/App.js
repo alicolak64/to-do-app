90560fbf4d96c53b15d9a02740c856b162965db2
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-08-31 13:33:17 +0300

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 
+import ToDoApp from './Components/ToDoApp/ToDoApp';
+import Footer from './Components/Footer/Footer';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src = 'https://to-do-cdn.microsoft.com/webapp/38cce120a00363d6d88c924546055c873aae9c159c1b4d9ec724687af6112fd9/preview.png' className="App-logo" alt="logo" />
-        <br />
-        <br />
-        <h3> To Do App</h3>
-      </header>
+
+      <ToDoApp />
+
+      <Footer/>
+      
     </div>
   );
 }
