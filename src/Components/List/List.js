605ca1e6078aae696572ba71c3f139798776110c
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-08-31 13:33:50 +0300

    Added List Component

---
+++ src/Components/List/List.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import './List.css'
+
+function List() {
+    return (
+        <section className="main">
+            <input className="toggle-all" type="checkbox" />
+            <label for="toggle-all">
+                Mark all as complete
+            </label>
+
+            <ul className="todo-list">
+                <li className="completed">
+                    <div className="view">
+                        <input className="toggle" type="checkbox" />
+                        <label>Learn JavaScript</label>
+                        <button className="destroy"></button>
+                    </div>
+                </li>
+                <li>
+                    <div className="view">
+                        <input className="toggle" type="checkbox" />
+                        <label>Learn React</label>
+                        <button className="destroy"></button>
+                    </div>
+                </li>
+                <li>
+                    <div className="view">
+                        <input className="toggle" type="checkbox" />
+                        <label>Have a life!</label>
+                        <button className="destroy"></button>
+                    </div>
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+export default List
