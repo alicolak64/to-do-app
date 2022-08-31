d05ae5b3862bdaf42fcb40ced271df017bc56c4a
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-08-31 11:59:33 +0300

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src = 'https://to-do-cdn.microsoft.com/webapp/38cce120a00363d6d88c924546055c873aae9c159c1b4d9ec724687af6112fd9/preview.png' className="App-logo" alt="logo" />
+        <br />
+        <br />
+        <h3> To Do App</h3>
       </header>
     </div>
   );
